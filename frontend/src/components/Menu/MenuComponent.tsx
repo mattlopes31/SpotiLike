fd167394760b4e79e951eb490078fe6a7ea63259
RemyEroes <113907739+RemyEroes@users.noreply.github.com>
2025-12-16 13:23:40 +0100

    feat: animations menu

--- frontend/src/components/Menu/MenuComponent.tsx
+++ frontend/src/components/Menu/MenuComponent.tsx
@@ -1,15 +1,45 @@
-import React from 'react'
+import { useState } from 'react';
 import '../../style/Menu.scss'
+import { motion, AnimatePresence } from 'framer-motion';
 
 function MenuComponent() {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const initialStyle = { opacity: 0, y: -10, scale: 0.8 };
+  const animateStyle = { opacity: 1, y: 0, scale: 1 };
+  const exitStyle = { opacity: 0, y: -10, scale: 0.8 };
+
   return (
     <nav>
-      <div className='logo'>
+      <motion.div
+        className='logo'
+        onClick={() => setShowMenu(!showMenu)}
+        whileTap={{ scale: 0.95 }}
+
+      >
         <img src="/spotilike.svg" alt="spotilike" />
         <h1>SpotiLike</h1>
-      </div>
-      <a href="/home">Home</a>
-      <a href="/search">Search</a>
+      </motion.div>
+      {showMenu && (
+        <AnimatePresence>
+          <motion.a
+            href="/profile"
+            initial={initialStyle}
+            animate={animateStyle}
+            exit={exitStyle}
+            transition={{ duration: 0.3, ease: [0.34, 1.56, 0.64, 1] }}
+          >Albums</motion.a>
+
+          <motion.a
+            href="/profile"
+            initial={initialStyle}
+            animate={animateStyle}
+            exit={exitStyle}
+            transition={{ duration: 0.3, ease: [0.34, 1.56, 0.64, 1], delay: 0.05 }}
+          >Artists</motion.a>
+
+        </AnimatePresence>
+      )}
     </nav>
   )
 }
